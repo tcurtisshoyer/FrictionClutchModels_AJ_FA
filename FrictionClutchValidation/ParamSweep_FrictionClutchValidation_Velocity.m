c1e6257e1f57c1e0a31f3c3b50d326ebac1b8d83
tcs34 <tcs34@duke.edu>
2023-11-02 10:42:18 -0400

    Upload New File

---
+++ FrictionClutchValidation/ParamSweep_FrictionClutchValidation_Velocity.m
@@ -0,0 +1,356 @@
+% This script perfoms a set of simulations to validate the implemetnation of the Friction Clutch
+% model in which the model is put into a "Validation Configuration"
+% consisting of linkages with a single bond interface having an ideal or
+% Bell Model slip bond characteristic. The parameter speed V0 is swept. The
+% external spring constant Kext is set to an arbitrarily high value
+% corresponding to an external surface that is much stiffer than the
+% linkage ensemble (Kext >> Klink*Nlink).
+%
+% This is implemented using the AJ Friction Clutch simulator
+% ("runSimulation_AJClutch.m") as follows. Use Type 1 Linkage
+% (non-reinforced), set Cad:Cad bond to permanent bond, and then set
+% aCat:Factin bond to ideal or Bell Model slip bond (i.e. only one-bound
+% state used).
+%   1: Ideal Bond, koff = 10/s
+%   2: Ideal Bond, koff = 5/s
+%   3: Bell Slip Bond, koff0 = 10/s, Fb = 5 pN
+%   4: Bell Slip Bond, koff0 = 5/s, Fb = 5 pN
+%   5: Bell Slip Bond, koff0 = 10/s, Fb = 10 pN
+%   Note: For all cases, kon = 2/s
+%
+% Refer to Supplementary Note 1 of Shoyer et al. and to the comprehensively
+% commented function "runSimulation_AJClutch.m" for complete documentation.
+%
+
+close all; clear all; clc; 
+
+%% Setup Output Directory
+ParentDir = 'G:\AJ_FA_ClutchModels_Demo';
+
+ModelType = 'FrictionClutch_Validation';
+StudyType = 'VelocitySweep';
+
+ChildDir = [ModelType '_' StudyType];
+OutDir = fullfile(ParentDir,ChildDir);
+if ~exist(OutDir)
+    mkdir(OutDir)
+end
+
+%% Simulation Params (see runSimulation_AJClutch.m for complete details)
+% SimulationParams: A data structure storing params that control how the
+% model is simulated and how simulation data is collected; has the
+% following fields:
+%   tend: total simulation time [s]
+%   maxAllowedTimeStep: max allowed timestep [s] in Gillespie SSA. If dt
+%       larger than this value, advance time and update mechanics without
+%       executing a linkage transition.
+%   interval_LogLinkageData: time step [s] for logging timecourse data
+%       on the clutch ensemble (DataLog_ClutchEnsemble) and on the state of
+%       a single linkage (DataLog_SingleLinkage).
+%   timeSpan_LogTransitionData: time interval [start end] in [s] for
+%       logging linkage state transitions (DataLog_Transitions).
+%
+% Read documentation in "runSimulation_AJClutch.m" to properly set these
+% simulation parameters. For a given parameter combination, the user should
+% verify that the results with the SSA match those obtained using a
+% discrete time-step algorithm with a sufficiently small time-step.
+SimulationParams.tend = 250;
+SimulationParams.interval_LogLinkageData = 1E-3; 
+SimulationParams.maxAllowedTimeStep = .001;
+SimulationParams.timeSpan_LogTransitionData = [.5 1]*SimulationParams.tend;
+
+%% Base Model Parameters (see runSimulation_AJClutch.m for complete details)
+% ModelParams: A data structure storing params for the model; has the
+% following fields:
+%
+%   V0: speed [nm/s]. For friction clutch, the relative speed between
+%       the cell (actin cytoskeleton) and the external surface (adjacent
+%       cell for AJ and substrate for FA).
+%
+%   springConstExternal: Spring constant [pN/nm] representing the stiffness
+%       of the external surface. For FA model, this is the substrate. For
+%       AJ model, this is the adjacent cell.
+%
+%   springConstLinkage: Spring constant [pN/nm] representing the stiffness
+%   of a single linkage.
+%
+%   RateConstants: Structure array storing the kinetic rate constants for
+%   all binding/unbinding state transitions.
+%           1: Cadherin:Cadherin [binding/unbinding]
+%           2: a-Catenin:F-actin [binding/unbinding]
+%           3: Vinculin(Vt):F-actin [binding/unbinding]
+%           4: Vinculin(Vh):a-Catenin [binding/unbinding]
+%       See "DefineForceDepRateConstantModels.m" for definition of
+%       transition rate constant models.
+%
+%   numLinkagesByType: 1x3 double specifying the number of each type of
+%   linkage in the clutch ensemble, i.e. [numLinkagesType1,
+%   numLinkagesType2, numLinkagesType3] where:
+%           Linkage Type 1 = Non-reinforced, i.e. aCat:Factin is only bond
+%               at Factin interface --> Cad:Cad-aCat:Factin)
+%           Linkage Type 2 = Vinculin Only, i.e. Vcl:Factin is only bond at
+%               Factin interface --> Cad:Cad-aCat:Vcl:Factin
+%           Linkage Type 3 = Reinforced, i.e. both aCat:Factin and
+%               Vcl:Factin bonds at Factin interface -->
+%               Cad:Cad-aCat:(Factin AND Vcl:Factin)
+%
+%   InitialStates: Structure containing the initial state for each
+%       bond/domain for each of the three linkage types. Each of the
+%       following fields is a 1x3 double storing the state for
+%       LinkageType1, LinkageType2, and LinkageType3, respectively.
+%           Bond_CadCad: State of Cad:Cad bond. 0=unbound, 1=bound.
+%           Bond_aCatFactin: State of aCat:Factin bond. 0=unbound, 1=bound.
+%           Bond_VclFactin: State of Vcl:Factin bond. 0=unbound, 1=bound.
+%           Bond_VclaCat: State of Vcl:aCat bond. 0=unbound, 1=bound.
+
+% Mechanics Parameters
+BaseParams.springConstExternal = [1E6]; % Kext [pN/nm] Set to arbitrarily stiff (Kext>>Nlinks*Klink) for validation configuration.
+BaseParams.numTotalLinkages = [50];
+BaseParams.springConstLinkage = [5];
+BaseParams.V0 = [10*1E3/60/60]; % V0 [nm/s] {10 um/hr ~ typical cell speed for collectively migrating MDCK cells --> Estimate for cell-cell relative motion}
+
+% Unbinding RateConstants
+% Import Force-Dependent Rate Constant Models
+%   RateConstants: Structure array storing the kinetic rate constants for
+%   all binding/unbinding state transitions.
+%           1: Cadherin:Cadherin [binding/unbinding]
+%           2: a-Catenin:F-actin [binding/unbinding]
+%           3: Vinculin(Vt):F-actin [binding/unbinding]
+%           4: Vinculin(Vh):a-Catenin [binding/unbinding]
+%       See "DefineForceDepRateConstantModels.m" for definition of
+%       transition rate constant models.
+DefineForceDepRateConstantModels_AJ;
+% Here are the bond models used for the AJ Friction Clutch Model:
+% BaseParams.RateConstants{1} = Ecad_Ecad_WT_DudkoHummerSzaboParams;
+% BaseParams.RateConstants{2} = aCatFactin_TwoBoundStateParams;
+% BaseParams.RateConstants{3} = Vcl_Factin_NegPull_TwoBoundStateParams;
+% BaseParams.RateConstants{4} = Vcl_aCat_BellModelWithMaxLifetimeLimitParams;
+
+% To implement validation configuration, use Type 1 Linkage
+% (non-reinforced), set Cad:Cad bond to permanent bond, and then set
+% aCat:Factin bond to ideal or Bell Model slip bond (i.e. only one-bound
+% state used).
+%   1: Ideal Bond, koff = 10/s
+%   2: Ideal Bond, koff = 5/s
+%   3: Bell Slip Bond, koff0 = 10/s, Fb = 5 pN
+%   4: Bell Slip Bond, koff0 = 5/s, Fb = 5 pN
+%   5: Bell Slip Bond, koff0 = 10/s, Fb = 10 pN
+%   Note: For all cases, kon = 2/s 
+BaseParams.RateConstants{1} = PermanentBond_DudkoHummerSzaboParams; % Set to Permanent Bond (defined in DefineForceDepRateConstantModels_AJ.m)
+% Define BaseParams.RateConstants{2} in parameter sweep below using one of the five generic bond models created here.
+BaseParams.RateConstants{3} = PermanentBond_TwoBoundStateParams; % Does not matter because using Type 1 Linkage for Validation Configuration
+BaseParams.RateConstants{4} = PermanentBond_BellModelWithMaxLifetimeLimitParams; % Does not matter because using Type 1 Linkage for Validation Configuration
+
+% Bond Models to be used for BaseParams.RateConstants{2} in parameter sweep below:
+% 1: Ideal Bond, koff = 10/s
+koff0 = 10; % Intrinsic Off-Rate
+IdealBond01_TwoBoundStateParams.Type = 'Two Bound State Catch';
+IdealBond01_TwoBoundStateParams.k0_10 = koff0; % k0_ij for 1 --> 0
+IdealBond01_TwoBoundStateParams.k0_20 = 0; % k0_ij for 2 --> 0
+IdealBond01_TwoBoundStateParams.k0_12 = 0; % k0_ij for 1 --> 2
+IdealBond01_TwoBoundStateParams.k0_21 = 0; % k0_ij for 2 --> 1
+IdealBond01_TwoBoundStateParams.x10 = 0; % xij for 1 --> 0
+IdealBond01_TwoBoundStateParams.x20 = 0; % xij for 2 --> 0
+IdealBond01_TwoBoundStateParams.x12 = 0; % xij for 1 --> 2
+IdealBond01_TwoBoundStateParams.x21 = 0; % xij for 2 --> 1
+% 2: Ideal Bond, koff = 5/s
+koff0 = 5; % Intrinsic Off-Rate
+IdealBond02_TwoBoundStateParams.Type = 'Two Bound State Catch';
+IdealBond02_TwoBoundStateParams.k0_10 = koff0; % k0_ij for 1 --> 0
+IdealBond02_TwoBoundStateParams.k0_20 = 0; % k0_ij for 2 --> 0
+IdealBond02_TwoBoundStateParams.k0_12 = 0; % k0_ij for 1 --> 2
+IdealBond02_TwoBoundStateParams.k0_21 = 0; % k0_ij for 2 --> 1
+IdealBond02_TwoBoundStateParams.x10 = 0; % xij for 1 --> 0
+IdealBond02_TwoBoundStateParams.x20 = 0; % xij for 2 --> 0
+IdealBond02_TwoBoundStateParams.x12 = 0; % xij for 1 --> 2
+IdealBond02_TwoBoundStateParams.x21 = 0; % xij for 2 --> 1
+% 3: Bell Slip Bond, koff0 = 10/s, Fb = 5 pN
+koff0 = 10; % Intrinsic Off-Rate
+Fb = 5; % Coefficent [pN] in Bell Model Slip Bond: koff = koff0*exp(F/Fb)
+kT = 4.114; % [pN*nm] at T = 298 K
+xb = kT/Fb; % Coefficent [nm] in Bell Model Slip Bond: koff = koff0*exp(F*xb/kT)
+SlipBond01_TwoBoundStateParams.Type = 'Two Bound State Catch';
+SlipBond01_TwoBoundStateParams.k0_10 = koff0; % k0_ij for 1 --> 0
+SlipBond01_TwoBoundStateParams.k0_20 = 0; % k0_ij for 2 --> 0
+SlipBond01_TwoBoundStateParams.k0_12 = 0; % k0_ij for 1 --> 2
+SlipBond01_TwoBoundStateParams.k0_21 = 0; % k0_ij for 2 --> 1
+SlipBond01_TwoBoundStateParams.x10 = xb; % xij for 1 --> 0
+SlipBond01_TwoBoundStateParams.x20 = 0; % xij for 2 --> 0
+SlipBond01_TwoBoundStateParams.x12 = 0; % xij for 1 --> 2
+SlipBond01_TwoBoundStateParams.x21 = 0; % xij for 2 --> 1
+% 4: Bell Slip Bond, koff0 = 10/s, Fb = 5 pN
+koff0 = 5; % Intrinsic Off-Rate
+Fb = 5; % Coefficent [pN] in Bell Model Slip Bond: koff = koff0*exp(F/Fb)
+kT = 4.114; % [pN*nm] at T = 298 K
+xb = kT/Fb; % Coefficent [nm] in Bell Model Slip Bond: koff = koff0*exp(F*xb/kT)
+SlipBond02_TwoBoundStateParams.Type = 'Two Bound State Catch';
+SlipBond02_TwoBoundStateParams.k0_10 = koff0; % k0_ij for 1 --> 0
+SlipBond02_TwoBoundStateParams.k0_20 = 0; % k0_ij for 2 --> 0
+SlipBond02_TwoBoundStateParams.k0_12 = 0; % k0_ij for 1 --> 2
+SlipBond02_TwoBoundStateParams.k0_21 = 0; % k0_ij for 2 --> 1
+SlipBond02_TwoBoundStateParams.x10 = xb; % xij for 1 --> 0
+SlipBond02_TwoBoundStateParams.x20 = 0; % xij for 2 --> 0
+SlipBond02_TwoBoundStateParams.x12 = 0; % xij for 1 --> 2
+SlipBond02_TwoBoundStateParams.x21 = 0; % xij for 2 --> 1
+% 5: Bell Slip Bond, koff0 = 10/s, Fb = 5 pN
+koff0 = 10; % Intrinsic Off-Rate
+Fb = 10; % Coefficent [pN] in Bell Model Slip Bond: koff = koff0*exp(F/Fb)
+kT = 4.114; % [pN*nm] at T = 298 K
+xb = kT/Fb; % Coefficent [nm] in Bell Model Slip Bond: koff = koff0*exp(F*xb/kT)
+SlipBond03_TwoBoundStateParams.Type = 'Two Bound State Catch';
+SlipBond03_TwoBoundStateParams.k0_10 = koff0; % k0_ij for 1 --> 0
+SlipBond03_TwoBoundStateParams.k0_20 = 0; % k0_ij for 2 --> 0
+SlipBond03_TwoBoundStateParams.k0_12 = 0; % k0_ij for 1 --> 2
+SlipBond03_TwoBoundStateParams.k0_21 = 0; % k0_ij for 2 --> 1
+SlipBond03_TwoBoundStateParams.x10 = xb; % xij for 1 --> 0
+SlipBond03_TwoBoundStateParams.x20 = 0; % xij for 2 --> 0
+SlipBond03_TwoBoundStateParams.x12 = 0; % xij for 1 --> 2
+SlipBond03_TwoBoundStateParams.x21 = 0; % xij for 2 --> 1
+
+% Binding Rate Constants
+BaseParams.RateConstants{1}.kon = [1E12]; % on rate for Ecad_Ecad. Set to permanent bond for validation configuration.
+BaseParams.RateConstants{2}.k01 = [2]; % on rate for aCat_Factin. Set to kon = 2/s
+BaseParams.RateConstants{2}.k02 = [0]; % No transition from unbound to Strong Bound State in 2-Bound-State Model
+BaseParams.RateConstants{3}.k01 = [0]; % on rate for Vcl_Factin. Turn off for validation configuration.
+BaseParams.RateConstants{3}.k02 = [0]; % No transition from unbound to Strong Bound State in 2-Bound-State Model
+BaseParams.RateConstants{4}.kon = [0]; % on rate for Vcl_aCat. Turn off for validation configuration.
+   
+% Set Number of each type of linkage
+%   numLinkagesByType: 1x3 double specifying the number of each type of
+%   linkage in the clutch ensemble, i.e. [numLinkagesType1,
+%   numLinkagesType2, numLinkagesType3] where:
+%           Linkage Type 1 = Non-reinforced, i.e. aCat:Factin is only bond
+%               at Factin interface --> Cad:Cad-aCat:Factin)
+%           Linkage Type 2 = Vinculin Only, i.e. Vcl:Factin is only bond at
+%               Factin interface --> Cad:Cad-aCat:Vcl:Factin
+%           Linkage Type 3 = Reinforced, i.e. both aCat:Factin and
+%               Vcl:Factin bonds at Factin interface -->
+%               Cad:Cad-aCat:(Factin AND Vcl:Factin)
+BaseParams.fractionLinkageType1 = [1]; % fraction of linkages that are Type 1
+BaseParams.fractionLinkageType2 = [0]; % fraction of linkages that are Type 2
+BaseParams.fractionLinkageType3 = [0]; % fraction of linkages that are Type 3
+BaseParams.numLinkagesByType(1) = round(BaseParams.numTotalLinkages*BaseParams.fractionLinkageType1);
+BaseParams.numLinkagesByType(2) = round(BaseParams.numTotalLinkages*BaseParams.fractionLinkageType2);
+BaseParams.numLinkagesByType(3) = round(BaseParams.numTotalLinkages*BaseParams.fractionLinkageType3); 
+
+%% Initial Conditions
+% Summary: All linkages are started with all bonds unbound, meaning 0
+% linkages are engaged.
+%
+% Initial State Definitions for Each Link Type
+% State for [LinkType1 LinkType2 LinkType3]
+%           Linkage Type 1 = Non-reinforced, i.e. aCat:Factin is only bond
+%               at Factin interface --> Cad:Cad-aCat:Factin)
+%           Linkage Type 2 = Vinculin Only, i.e. Vcl:Factin is only bond at
+%               Factin interface --> Cad:Cad-aCat:Vcl:Factin
+%           Linkage Type 3 = Reinforced, i.e. both aCat:Factin and
+%               Vcl:Factin bonds at Factin interface -->
+%               Cad:Cad-aCat:(Factin AND Vcl:Factin)
+BaseParams.InitialStates.Bond_CadCad = [0 0 0];
+BaseParams.InitialStates.Bond_aCatFactin = [0 0 0];
+BaseParams.InitialStates.Bond_VclFactin = [0 0 0];
+BaseParams.InitialStates.Bond_VclaCat = [0 0 0];
+
+%% Parameter Sweep
+V0_Sweep = [logspace(-1,0,5) 2:2:8 10:10:100 125 150 200]; % 2x the velocity sweep for AJ and FA Friction Clutch Models
+
+baseSimID = 0; % Each V0 value has a unique baseSimID
+simID = 0; % Each simulation (V0 value + linkage configuration) has a unique simID
+for V0 = V0_Sweep % Velocity Sweep
+
+    baseSimID = baseSimID + 1;
+    
+    % Set Model Params to Base Params
+    ModelParams = BaseParams;
+    
+    % Set V0 to current sweep value
+    ModelParams.V0 = V0;
+    
+    % For each value of above defined parameter sweep, simulate multiple
+    % different linkage configurations:
+    %   1: Ideal Bond, koff = 10/s
+    %   2: Ideal Bond, koff = 5/s
+    %   3: Bell Slip Bond, koff0 = 10/s, Fb = 5 pN
+    %   4: Bell Slip Bond, koff0 = 5/s, Fb = 5 pN
+    %   5: Bell Slip Bond, koff0 = 10/s, Fb = 10 pN  
+    for linkConfigSweepID = 1:5
+        
+        simID = simID + 1;
+        
+        % Setup linkage configuration as defined above.
+        if linkConfigSweepID==1
+            % Set unbinding rate constant model defined above.
+            ModelParams.RateConstants{2} = IdealBond01_TwoBoundStateParams;
+            % Set kon = 2/s
+            ModelParams.RateConstants{2}.k01 = [2]; % on rate for aCat_Factin. Set to kon = 2/s.
+            ModelParams.RateConstants{2}.k02 = [0]; % No transition from unbound to Strong Bound State in 2-Bound-State Model
+        elseif linkConfigSweepID==2
+            % Set unbinding rate constant model defined above.
+            ModelParams.RateConstants{2} = IdealBond02_TwoBoundStateParams;
+            % Set kon = 2/s
+            ModelParams.RateConstants{2}.k01 = [2]; % on rate for aCat_Factin. Set to kon = 2/s.
+            ModelParams.RateConstants{2}.k02 = [0]; % No transition from unbound to Strong Bound State in 2-Bound-State Model
+        elseif linkConfigSweepID==3
+            % Set unbinding rate constant model defined above.
+            ModelParams.RateConstants{2} = SlipBond01_TwoBoundStateParams;
+            % Set kon = 2/s
+            ModelParams.RateConstants{2}.k01 = [2]; % on rate for aCat_Factin. Set to kon = 2/s.
+            ModelParams.RateConstants{2}.k02 = [0]; % No transition from unbound to Strong Bound State in 2-Bound-State Model
+        elseif linkConfigSweepID==4
+            % Set unbinding rate constant model defined above.
+            ModelParams.RateConstants{2} = SlipBond02_TwoBoundStateParams;
+            % Set kon = 2/s
+            ModelParams.RateConstants{2}.k01 = [2]; % on rate for aCat_Factin. Set to kon = 2/s.
+            ModelParams.RateConstants{2}.k02 = [0]; % No transition from unbound to Strong Bound State in 2-Bound-State Model
+        elseif linkConfigSweepID==5
+            % Set unbinding rate constant model defined above.
+            ModelParams.RateConstants{2} = SlipBond03_TwoBoundStateParams;
+            % Set kon = 2/s
+            ModelParams.RateConstants{2}.k01 = [2]; % on rate for aCat_Factin. Set to kon = 2/s.
+            ModelParams.RateConstants{2}.k02 = [0]; % No transition from unbound to Strong Bound State in 2-Bound-State Model
+        end
+        
+        % Run Simulation
+        fileCheck = dir(fullfile(OutDir,['Data_simID_' num2str(simID) '.mat']));
+        if isempty(fileCheck)
+            tic;
+            [DataLog_ClutchEnsemble,DataLog_SingleLinkage,DataLog_Transitions,AllVariableNames,SimulationPerformanceStats] = runSimulation_AJClutch(ModelParams,SimulationParams);
+            t=toc;
+            disp(['simID ' num2str(simID) ' complete in ' num2str(t) ' sec.']);
+            % Save simulation data
+            SimulationPerformanceStats.computerTime = t;
+            save(fullfile(OutDir,['Data_simID_' num2str(simID) '.mat']), 'DataLog_ClutchEnsemble', 'DataLog_SingleLinkage', 'DataLog_Transitions', 'AllVariableNames', 'SimulationPerformanceStats', 'ModelParams', 'SimulationParams');   
+        end
+        
+        % Make record of parameters
+        numTotalLinkages = ModelParams.numTotalLinkages;
+        fractionLinkageType1 = ModelParams.fractionLinkageType1;
+        fractionLinkageType2 = ModelParams.fractionLinkageType2;
+        fractionLinkageType3 = ModelParams.fractionLinkageType3;
+        % V0 set by for loop. V0 = ModelParams.V0;
+        springConstExternal = ModelParams.springConstExternal;
+        springConstLinkage = ModelParams.springConstLinkage;
+        
+        kon = ModelParams.RateConstants{2}.k01; % On Rate Constant
+        koff0 = ModelParams.RateConstants{2}.k0_10; % Intrinsic Off-Rate
+        xb = ModelParams.RateConstants{2}.x10; % Coefficent [nm] in Bell Model Slip Bond: koff = koff0*exp(F*xb/kT)
+        Fb = kT/xb; % Coefficent [pN] in Bell Model Slip Bond: koff = koff0*exp(F/Fb)
+
+        ParamSweep(simID,:) = table(simID, baseSimID, linkConfigSweepID,...
+            numTotalLinkages,...
+            fractionLinkageType1,...
+            fractionLinkageType2,...
+            fractionLinkageType3,...
+            V0,...
+            springConstExternal,...
+            springConstLinkage,...
+            kon,...
+            koff0,...
+            xb,...
+            Fb);
+        
+    end
+    
+end
+
+save(fullfile(OutDir,'AllParamSweep.mat'), 'ParamSweep');
